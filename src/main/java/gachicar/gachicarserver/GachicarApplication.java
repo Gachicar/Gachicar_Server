33777887d5d46ee5facb0cd2c6de60ee2cd49cec
yeonsu <bestdustn@sookmyung.ac.kr>
2023-11-02 01:17:44 +0900

    rename folder

--- src/main/java/gachicar/gachicarserver/GachicarApplication.java
+++ src/main/java/gachicar/gachicarserver/GachicarApplication.java
@@ -1,4 +1,4 @@
-package gachicar.gachicar;
+package gachicar.gachicarserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/gachicar/gachicarserver/GachicarApplicationTests.java
+++ src/test/java/gachicar/gachicarserver/GachicarApplicationTests.java
@@ -1,4 +1,4 @@
-package gachicar.gachicar;
+package gachicar.gachicarserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
