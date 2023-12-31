f841da143a26e7925d2655738f5aa5f8db0d13c3
yeonsu <bestdustn@sookmyung.ac.kr>
2023-12-31 19:36:12 +0900

    [FIX] Update Car.java - 컬럼 추가: 주행거리, 주유상해, 기름유형, rating, 자주 가는 목적지, 시간, 차를 최근에 사용한 날짜

--- src/main/java/gachicar/gachicarserver/domain/Car.java
+++ src/main/java/gachicar/gachicarserver/domain/Car.java
@@ -5,6 +5,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.sql.Time;
+import java.util.Date;
 
 @Entity
 @Getter
@@ -28,4 +30,25 @@ public class Car {
     public Car(String carName) {
         this.carName = carName;
     }
+
+    @Setter
+    private Long distance;  // 주행거리
+
+    @Setter
+    private String oilStatus;  // 주유 상태
+
+    private String fuelType;    // fuelType
+
+    private int rating;     // rating
+
+    @Setter
+    private String location;    // location: 자주 가는 목적지
+
+    @Setter
+    private Long driveTime;     // 시간
+
+    @Setter
+    private Date latestDate;    // 날짜: 차를 최근에 사용한 날짜
+
+
 }
