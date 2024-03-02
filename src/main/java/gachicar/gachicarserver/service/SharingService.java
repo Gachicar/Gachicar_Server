51cfba8ce233c0d1cf8294ab853e583758ebae7c
yeonsu <bestdustn@sookmyung.ac.kr>
2024-03-02 14:07:13 +0900

    [FIX] Update RC car Socket 통신 - 소켓 통신으로 RC카 제어 성공 - 이전 코드 삭제

--- src/main/java/gachicar/gachicarserver/service/SharingService.java
+++ src/main/java/gachicar/gachicarserver/service/SharingService.java
@@ -40,79 +40,17 @@ public class SharingService {
         // 차량 상태를 사용 중 상태로 변경
         car.setCarStatus(Boolean.TRUE);
 
-        // RC카에 연결
-        Socket socket = connectCar(car);
-
         // 목적지로 주행 시작
         car.setCurLoc(requestDto.getDeparture());
-        OutputStream outputStream = drive(socket, "주행시작");
+        socketClient("시작");
 
         // 주행 완료 후 현재 위치를 목적지로 설정
         car.setCurLoc(requestDto.getDestination());
 
-        // RC카 연결 해제
-        disconnectCar(socket, outputStream);
     }
 
-    // 주행
-    public OutputStream drive(Socket socket, String message) {
-        try {
-            // RC카로 명령 보내기
-            OutputStream outputStream = socket.getOutputStream();
-            outputStream.write(message.getBytes());
-            outputStream.flush();
-            log.info("RC카 제어 요청이 성공적으로 전송되었습니다.");
-
-            return outputStream;
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            log.info("RC카 제어 요청 전송 중 오류가 발생했습니다.");
-            throw new ApiErrorException(ApiErrorStatus.SOCKET_ERROR);
-        }
-    }
-
-    // 공유차량 연결
-    public Socket connectCar(Car car) {
-        // RC카의 IP 주소와 포트 번호
-        String carIP = "localhost";     // RC_CAR_IP_ADDRESS
-        int carPort = 9091;     // RC_CAR_PORT_NUMBER
-
-        try {
-            // 서버 소켓 생성 및 포트 바인딩
-            ServerSocket serverSocket = new ServerSocket(carPort);
-
-            // 클라이언트 연결 대기
-            Socket clientSocket = serverSocket.accept();
-
-            return new Socket(carIP, carPort);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new ApiErrorException(ApiErrorStatus.CONNECT_FAILED);
-        }
-    }
-
-    // 공유차량 연결 해제
-    public void disconnectCar(Socket socket, OutputStream outputStream) {
-        try {
-            // 출력 스트림 닫기
-            outputStream.close();
-        } catch (IOException e) {
-            log.error("출력 스트림을 닫는 중 오류가 발생했습니다.", e);
-            throw new ApiErrorException(ApiErrorStatus.DISCONNECT_FAILED);
-        }
-
-        try {
-            // 소켓 닫기
-            socket.close();
-        } catch (IOException e) {
-            log.error("소켓을 닫는 중 오류가 발생했습니다.", e);
-            throw new ApiErrorException(ApiErrorStatus.DISCONNECT_FAILED);
-        }
-    }
-
-    public void sendMessage(){
-        int portNumber = 9999;
+    public void socketServer(){
+        int portNumber = 9595;
 
         try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
             System.out.println("Server is running on port " + portNumber);
@@ -139,9 +77,15 @@ public class SharingService {
         }
     }
 
+    /**
+     * [파이썬 소켓 서버와 통신]
+     * - RC카 주피터랩 ip 주소: 192.168.0.7
+     * - 접속 포트: 9851
+     * @param command : RC카에 보낼 명령어
+     */
     public void socketClient(String command) {
-        String serverAddress = "localhost"; // 대상 서버의 IP 주소
-        int serverPort = 9999; // 대상 서버의 포트 번호
+        String serverAddress = "192.168.0.7"; // 대상 서버의 IP 주소
+        int serverPort = 9851; // 대상 서버의 포트 번호
 
         try (
                 Socket socket = new Socket(serverAddress, serverPort);
